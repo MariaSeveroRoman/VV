e36fae6a30d9380e15bcdea8cfe8576c7477c92a
Carlos Cuesta Rios <47385151+CarlosCuestaRios@users.noreply.github.com>
2019-09-25 10:51:06 +0200

    Modificación primera hipotesis
    Cambios en la cabecera de la función y sustitución del bucle while por un for

--- BubbleSort.java
+++ BubbleSort.java
@@ -8,25 +8,33 @@ public class BubbleSort {
 	 * @param array The array to be sorted
 	 * @param size The count of total number of elements in array
 	 **/
+	public static void main(String[] args) {
+		int []datos={1,0,0,0,2,0};
+		System.out.println("Datos antes de ejecucion");
+		imprimir (datos);
+		BS(datos, datos.length);
+		System.out.println("\nDatos despues de ejecucion");
+		imprimir(datos);
+	}
+	
+	public static <T extends Comparable<T>> void BS(int array[], int size) {
+		int last = size;
 
-	public static <T extends Comparable<T>> void BS(T array[], int size) {
-		boolean swap;
-		int last = size - 1;
-		
-		do
-		{
-			swap = false;
 			for (int count = 0; count < last-1; count++)
 			{
-				int comp = array[count].compareTo(array[count + 1]);
-				if (comp > 0)
+				for(int j=0;j<last-count;j++)
+				if (array[count]>array[count+1])
 				{
-					T temp = array[count];
+					int temp = array[count];
 					array[count] = array[count + 1];
 					array[count + 1] = temp;
-					swap = true;
 				}
 			}
-		} while (swap);
+	}
+	
+	public static void imprimir(int []datos){
+		for(int i=0;i<datos.length;i++){
+			System.out.print(datos[i]);
+		}
 	}
 }
